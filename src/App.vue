4584ebb17fa9c18a5e65687cb206d9d75c9e4f85
Marius <marius.andreiana@gmail.com>
2021-08-13 17:44:41 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+ 
+  test
+</template>
+
+<script setup>
+import { provide } from 'vue'
+
+// const count = ref(1)
+// const plusOne = computed(() => count.value + 1)
+// export default {
+//     computed: {
+//         notification_width: function () {
+//             return Math.min(600, Math.max(document.documentElement.clientWidth, window.innerWidth || 0)) || 'px' //fit on mobile
+//         }
+//     },
+
+</script>
+
